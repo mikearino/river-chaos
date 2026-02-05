dcaa7fd911653cdf2bd1d47b0adcd0e47b705593
Mike Lofgren <mikeylofgren@gmail.com>
2026-02-04 21:32:31 -0800

    Add in cycling leaderboard functionality to start screen (#3)

--- vite-project/src/scenes/StartScene.js
+++ vite-project/src/scenes/StartScene.js
@@ -1,3 +1,5 @@
+import { getHighScores } from "../utils/scoreService";
+
 export default class StartScene extends Phaser.Scene {
   constructor() {
     super("StartScene");
@@ -31,16 +33,18 @@ export default class StartScene extends Phaser.Scene {
     this.bgm = this.sound.add("startMusic", { loop: true, volume: 0.5 });
     this.bgm.play();
 
-    this.add
+    // store objects to hide them later
+    this.titleText = this.add
       .text(640, 300, "River Chaos", { fontSize: "64px", color: "#000" })
       .setOrigin(0.5);
-    const prompt = this.add
+
+    this.promptText = this.add
       .text(640, 400, "Press SPACE to start", {
         fontSize: "32px",
         color: "#000",
       })
       .setOrigin(0.5);
-    this.add
+    this.insructionsText = this.add
       .text(640, 355, "Use the Arrow Keys to Play", {
         fontSize: "32px",
         color: "#000",
@@ -48,13 +52,15 @@ export default class StartScene extends Phaser.Scene {
       .setOrigin(0.5);
 
     //toggle vis every 500ms
-    this.time.addEvent({
+    this.blinkEvent = this.time.addEvent({
       delay: 500,
       loop: true,
       callback: () => {
-        prompt.visible = !prompt.visible;
+        this.promptText.visible = !this.promptText.visible;
       },
     });
+
+    this.startTexts = [this.titleText, this.promptText, this.insructionsText];
 
     this.input.keyboard.once("keydown-SPACE", () => {
       if (this.bgm) {
@@ -62,6 +68,81 @@ export default class StartScene extends Phaser.Scene {
         this.sound.remove(this.bgm);
       }
       this.scene.start("GameScene");
+    });
+
+    this.time.delayedCall(2000, () => {
+      this.hideStartText();
+      this.loadAndCycle();
+    });
+  }
+
+  hideStartText() {
+    this.startTexts.forEach((t) => t.setVisible(false));
+    this.blinkEvent.paused = true;
+  }
+
+  showStartText() {
+    this.startTexts.forEach((t) => t.setVisible(true));
+    this.blinkEvent.paused = false;
+  }
+
+  hideScoreText() {
+    if (!this.scoreTexts) return;
+    this.scoreTexts.forEach((t) => t.destroy());
+    this.scoreTexts = [];
+  }
+
+  showScoreText() {
+    this.scoreTexts.forEach((t) => t.setVisible(true));
+  }
+
+  async showHighScores() {
+    const scores = await getHighScores();
+
+    //clear when toggled
+    this.scoreTexts = [];
+
+    this.scoreTexts.push(
+      this.add
+        .text(640, 260, "High Scores", {
+          fontSize: "36px",
+          color: "#fff",
+        })
+        .setOrigin(0.5),
+    );
+
+    scores.slice(0, 5).forEach((entry, index) => {
+      const scoreText = this.add
+        .text(640, 320 + index * 40, `${entry.initials}: ${entry.score}`, {
+          fontSize: "28px",
+          color: "#fff",
+        })
+        .setOrigin(0.5);
+      this.scoreTexts.push(scoreText);
+    });
+
+    this.showScoreText();
+
+    //schedule back to the start
+    this.time.delayedCall(3000, () => {
+      this.hideScoreText();
+      this.showStartText();
+    });
+  }
+
+  async loadAndCycle() {
+    await this.showHighScores();
+
+    //wait 3 secs, go back to start text
+    this.time.delayedCall(3000, () => {
+      this.hideScoreText();
+      this.showStartText();
+
+      //wait another 3 secs and cycle again
+      this.time.delayedCall(3000, () => {
+        this.hideStartText();
+        this.loadAndCycle();
+      });
     });
   }
 }
